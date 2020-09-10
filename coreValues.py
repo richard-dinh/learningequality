804555ba5d8898add080c4a1c1c1b3b45841d5bb
richard-dinh <richarddinh@gmail.com>
2020-09-10 09:50:11 -0700

    added comments

--- coreValues.py
+++ coreValues.py
@@ -6,13 +6,18 @@ from bs4 import BeautifulSoup
 import os
 
 def scrapeCoreValuesPage(htmlUrl, cssUrl, bootstrapUrl):
+  # GET request for html data then parsing the data
   htmlResponse = requests.get(htmlUrl, timeout=10)
   htmlSoup = BeautifulSoup(htmlResponse.content, 'html.parser')
+  # GET request for CSS file data then parsing the data
   cssResponse = requests.get(cssUrl, timeout=10)
   cssSoup = BeautifulSoup(cssResponse.content, 'html.parser')
+  # GET request for bootstrap styling data then parsing the data
   bootstrapResponse = requests.get(bootstrapUrl, timeout=10)
   bootstrapSoup = BeautifulSoup(bootstrapResponse.content, 'html.parser')
+  # Extracting only Core Values text
   coreValuesInfo = htmlSoup.find('div', {'id': 'about-body'})
+  # Generating the HTML markup
   htmlMarkUp = """<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,22 +35,28 @@ def scrapeCoreValuesPage(htmlUrl, cssUrl, bootstrapUrl):
   </div>
 </body>
 </html>""".format(coreValuesInfo)
-  print(coreValuesInfo)
+  # format the CSS and Bootstrap parsed data into strings
   cssMarkUp = '{}'.format(cssSoup)
   bootstrapMarkup = '{}'.format(bootstrapSoup)
+  
   createMarkUp(htmlMarkUp, cssMarkUp, bootstrapMarkup)
   return
 
 def createMarkUp(htmlMarkUp, cssMarkUp, bootstrapMarkup):
+  # Create directory public to hold data files
   if not os.path.exists('public'):
     os.makedirs('public')
+  # Get current working directory
   cwd = os.getcwd()
+  # Create index.html in public and write to it
   writer = open('{}/public/index.html'.format(cwd), 'w')
   writer.write(htmlMarkUp)
   writer.close()
+  # Create styles.css in public and write to it
   writer = open('{}/public/styles.css'.format(cwd), 'w')
   writer.write(cssMarkUp)
   writer.close()
+  # Create bootstrap.min.css in public and write to it
   writer = open('{}/public/bootstrap.min.css'.format(cwd), 'w')
   writer.write(bootstrapMarkup)
   writer.close()
